37247e8fba92e215ac8fcfa072d33773aed2269e
Wizardy <wizardhassan99@gmail.com>
2025-04-20 19:14:18 +0100

    commit

---
+++ frontend/App.tsx
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { Provider as PaperProvider } from "react-native-paper"
+import { AuthProvider } from "./src/context/AuthContext"
+import AppNavigator from "./src/navigation/AppNavigator"
+import { theme } from "./src/theme/theme"
+import { StatusBar } from "expo-status-bar"
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <StatusBar style="dark" />
+      <AuthProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </PaperProvider>
+  )
+}
