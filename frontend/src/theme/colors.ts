a7fc2615729a6ef599a7aada46c05264f825c05f
Wizardy <wizardhassan99@gmail.com>
2025-04-20 19:15:36 +0100

    commit

---
+++ frontend/src/theme/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  black: "#000000",
+  lightBlueGray: "#94B4C1",
+  lavender: "#8E7DBE",
+  royalBlue: "#3A59D1",
+  lightPeriwinkle: "#9FB3DF",
+  darkNavy: "#123458",
+  lightGray: "#DBDBDB",
+  white: "#FFFFFF",
+  error: "#FF3B30",
+  success: "#34C759",
+}
