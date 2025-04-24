51fb255ee627da67f45241ea9db336f33d9f0356
Wizardy <wizardhassan99@gmail.com>
2025-04-24 12:55:02 +0100

    commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,7 +7,7 @@ const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "book_borrowing_app",
+  database: process.env.DB_NAME || "lib_bdb", //lib_bdb => backup database
 }
 
 const pool = mysql.createPool(dbConfig)
